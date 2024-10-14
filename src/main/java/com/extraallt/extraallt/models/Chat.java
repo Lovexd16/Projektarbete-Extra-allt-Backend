c27416c3d224b7b26f4e6275aafb8f12d57c95b6
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-10-14 12:02:10 +0200

    Fixat websocketfiler
    deploy

---
+++ src/main/java/com/extraallt/extraallt/models/Chat.java
@@ -0,0 +1,18 @@
+package com.extraallt.extraallt.models;
+
+public class Chat {
+    public String chat;
+
+    public Chat(String chat) {
+        this.chat = chat;
+    }
+
+    public String getChat() {
+        return chat;
+    }
+
+    public void setChat(String chat) {
+        this.chat = chat;
+    }
+
+}
---
+++ src/main/java/com/extraallt/extraallt/models/Hello.java
@@ -0,0 +1,17 @@
+package com.extraallt.extraallt.models;
+
+public class Hello {
+    private String content;
+
+    public Hello(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
