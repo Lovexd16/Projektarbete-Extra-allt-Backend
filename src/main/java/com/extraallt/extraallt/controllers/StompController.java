3b0a0cdbcf63fed2b76f1ef96296e8b7052d30bc
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-10-29 11:31:09 +0100

    Lite fix för chatt

--- src/main/java/com/extraallt/extraallt/controllers/StompController.java
+++ src/main/java/com/extraallt/extraallt/controllers/StompController.java
@@ -1,27 +1,18 @@
 package com.extraallt.extraallt.controllers;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-
-import com.extraallt.extraallt.models.Chat;
-import com.extraallt.extraallt.models.ChatMessage;
-import com.extraallt.extraallt.models.Hello;
-import com.extraallt.extraallt.models.HelloMessage;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
+@CrossOrigin("*")
 public class StompController {
-     @MessageMapping("/greet")
-    @SendTo("/topic/greeting")
-    public Hello hello(HelloMessage message) {
-        System.out.println("/Hello");
-        return new Hello(message.getName() + " gick in i chatten!");
-    }
 
     @MessageMapping("/chat")
     @SendTo("/topic/chat")
-    public Chat chat(ChatMessage chat) {
-        System.out.println("/chat");
-        return new Chat(chat.getContent());
+    public String sendChatMessage(@Payload String message) {
+        return message;
     }
 }
